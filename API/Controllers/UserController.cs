5fbb379773bf08ed2028571d09ea48c0814493a8
Robinson Souza <Robinsond76@gmail.com>
2020-11-24 14:27:25 -0500

    Added a User Controller and basic login functionality

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Payroll.Core;
+using Payroll.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserController(UserManager<AppUser> userManager, 
+            SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(UserLoginDto userDto)
+        {
+            var user = await _userManager.FindByEmailAsync(userDto.Email);
+
+            if (user == null)
+                return Unauthorized("Incorrect username or password");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, userDto.Password, false);
+
+            //Return a token
+            if (result.Succeeded)
+                return user;
+
+            return Unauthorized("Incorrect username or password.");
+        }
+    }
+}
---
+++ Payroll.Data/Models/UserLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Payroll.Data.Models
+{
+    public class UserLoginDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
