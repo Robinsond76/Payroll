c9cb271ac522680d13b216494082f2ce6e884f25
Robinson Souza <Robinsond76@gmail.com>
2020-11-24 12:43:47 -0500

    Seeded users to database

--- API/Startup.cs
+++ API/Startup.cs
@@ -33,6 +33,7 @@ namespace API
             var identityBuilder = new IdentityBuilder(builder.UserType, builder.Services);
             identityBuilder.AddEntityFrameworkStores<PayrollContext>();
             identityBuilder.AddSignInManager<SignInManager<AppUser>>();
+            services.AddAuthentication();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Payroll.Data/Persistence/Seed.cs
+++ Payroll.Data/Persistence/Seed.cs
@@ -1,15 +1,48 @@
-﻿using Payroll.Core;
+﻿using Microsoft.AspNetCore.Identity;
+using Payroll.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Payroll.Data.Persistence
 {
     public class Seed
     {
-        public static void SeedData(PayrollContext context)
+        public static async Task SeedData(PayrollContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser
+                    {
+                        DisplayName = "Bob",
+                        UserName = "bob",
+                        Email = "bob@test.com",
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Tom",
+                        UserName = "tom",
+                        Email = "tom@test.com",
+                    },
+                    new AppUser
+                    {
+                        DisplayName = "Jane",
+                        UserName = "jane",
+                        Email = "jane@test.com",
+                    }
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
+
             if(!context.Jobsites.Any()) //if no data in database...seed
             {
                 var jobsites = new List<Jobsite>
