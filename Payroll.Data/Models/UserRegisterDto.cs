3907be5524ad34d7d50569e14f1edad8f636603c
Robinson Souza <Robinsond76@gmail.com>
2020-11-30 09:07:11 -0500

    Added a password validator using regex to user creation

--- Payroll.Data/Models/UserRegisterDto.cs
+++ Payroll.Data/Models/UserRegisterDto.cs
@@ -10,11 +10,17 @@ namespace Payroll.Data.Models
         [Required]
         public string DisplayName { get; set; }
         [Required]
+        [StringLength( 10, 
+            ErrorMessage = "Username must be between 3 and 10 characters long", 
+            MinimumLength = 3)]
         public string Username { get; set; }
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         [MinLength(8, ErrorMessage = "Password must but a minimum of 8 characters long")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+         ErrorMessage = "Password Requires: 1 lowercase letter, 1 uppercase letter, 1 number, 1 symbol")]
         public string Password { get; set; }
     }
 }
