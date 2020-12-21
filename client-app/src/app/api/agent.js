2e9f4da4d485ad7100c3f6e14b10b83251332a82
Robinson Souza <Robinsond76@gmail.com>
2020-12-21 11:58:45 -0500

    added axios interceptor to attach token to request

--- client-app/src/app/api/agent.js
+++ client-app/src/app/api/agent.js
@@ -2,6 +2,17 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 const responseBody = (response) => response.data;
 
 const requests = {
